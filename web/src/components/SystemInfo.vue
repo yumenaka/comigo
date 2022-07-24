3930d2069c43a0ae0e87d60a95429780a4e1e3ef
yumenaka <www,bailin.tv@gmail.com>
2022-07-24 09:02:39 +0800

    web:Show SystemInfo

---
+++ web/src/components/SystemInfo.vue
@@ -0,0 +1,119 @@
+
+
+<template>
+  <div id="SystemInfo" class="w-4/6">
+    <n-space vertical>
+      <p>CPU:</p>
+      <n-progress type="circle" :percentage="cpu_percentage" />
+      <p>{{ this.$store.state.server_status.OSInfo.cpu_num_physical }}Core
+        {{ this.$store.state.server_status.OSInfo.cpu_num_logical_total }}Thread</p>
+      <n-divider></n-divider>
+
+      <p>RAM:</p>
+      <n-progress type="circle" :percentage="ram_percentage" />
+      <p>{{ (this.$store.state.server_status.OSInfo.memory_total*(this.$store.state.server_status.OSInfo.memory_used_percent/100)/ (1024 * 1024 * 1024)).toFixed(2) }}GB/{{ (this.$store.state.server_status.OSInfo.memory_total / (1024 * 1024 * 1024)).toFixed(2) }}GB</p>
+      <n-divider></n-divider>
+
+      <p>Books:{{ this.$store.state.server_status.NumberOfBooks }}</p>
+      <p>Upload:{{ this.$store.state.server_status.SupportUploadFile }}</p>
+      <!-- <p>Devices:{{ this.$store.state.server_status.NumberOfOnLineDevices }}</p> -->
+      <!-- <p>cpu_num_physical:{{this.$store.state.server_status.OSInfo.cpu_num_physical}}</p>
+      <p>cpu_num_logical_total:{{this.$store.state.server_status.OSInfo.cpu_num_logical_total}}</p> -->
+
+    </n-space>
+  </div>
+</template>
+
+<script>
+import { NProgress, useMessage, NSpace, NDivider, } from "naive-ui";
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  name: "AboutPage",
+  props: ['showSystemInfo'],
+  emits: ["setSome"],
+  components: {
+    NProgress,//进度条：https://www.naiveui.com/zh-CN/os-theme/components/progress
+    NSpace,
+    NDivider,//分割线
+  },
+  setup() {
+    const cpu_percentageRef = ref(0);
+    const ram_percentageRef = ref(0);
+    const message = useMessage()
+    return {
+      message,
+      cpu_percentage: cpu_percentageRef,
+      ram_percentage: ram_percentageRef,
+    }
+  },
+  data() {
+    return {
+      readerMode: "",
+      upLoadHint: "",
+    };
+  },
+  created() {
+    this.beforeBookNum = this.$store.state.server_status.NumberOfBooks;
+    this.$store.dispatch("syncSeverStatusDataAction");
+    this.showStatus();
+  },
+  //挂载前
+  beforeMount() {
+  },
+  onMounted() {
+  },
+  //卸载前
+  beforeUnmount() {
+  },
+  methods: {
+    //开始展示  api/getstatus
+    // ServerName	"Comigo v0.8.5"
+    // ServerHost	"192.168.3.145"
+    // ServerPort	1234
+    // NumberOfBooks	79
+    // NumberOfOnLineUser	1
+    // NumberOfOnLineDevices	1
+    // SupportUploadFile	true
+    // ClientIP	"127.0.0.1"
+
+    // OSInfo	
+    // cpu_num_logical_total	16
+    // cpu_num_physical	8
+    // cpu_used_percent	63.60414336345319
+    // memory_total	66889330688
+    // memory_free	14006677504
+    // memory_used_percent	36.62989823337489
+    // description	"linux amd64"
+
+    showStatus() {
+      // this.message.success("");
+      //每次上传完成后，触发轮询的次数
+      const pollTimer = setInterval(() => {
+        //服务器拉取最新状态，看是否新加了书籍
+        this.$store.dispatch("syncSeverStatusDataAction");
+        this.cpu_percentage = this.$store.state.server_status.OSInfo.cpu_used_percent.toFixed(2);
+        this.ram_percentage = this.$store.state.server_status.OSInfo.memory_used_percent.toFixed(2);
+        if (!this.showSystemInfo) {
+          clearInterval(pollTimer);
+        }
+      }, 1000);
+    },
+    onRefreshPage() {
+      location.reload();
+    },
+  },
+  computed: {
+  },
+});
+</script>
+
+<style scoped>
+#UploadFile {
+  padding: 20px;
+}
+
+.n-upload {
+  --tw-bg-opacity: 0.5;
+  background-color: rgba(249, 250, 251, var(--tw-bg-opacity));
+}
+</style>
--- web/src/views/UploadPage.vue
+++ web/src/views/UploadPage.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="UploadPage w-full h-screen flex flex-col">
         <Header class="header flex-none h-12" :bookIsFolder="false" :headerTitle="this.getUploadTitile()"
-            :showReturnIcon="true" :showSettingsIcon="false"  :bookID='null' :setDownLoadLink="false">
+            :showReturnIcon="true" :showSettingsIcon="true" :bookID='null' :setDownLoadLink="false"
+            @drawerActivate="this.drawerActivate">
         </Header>
         <!-- 渲染书架部分 有书的时候显示书  没有的时候显示上传控件-->
         <!-- Flex Grow 控制 flex 项目放大的功能类 https://www.tailwindcss.cn/docs/flex-grow -->
@@ -9,12 +10,22 @@
         <div class="mian_area flex-grow">
             <UploadFile>
             </UploadFile>
+
         </div>
+
         <Bottom class="bottom flex-none h-12" :softVersion="
             this.$store.state.server_status.ServerName
                 ? this.$store.state.server_status.ServerName
                 : 'Comigo'
         "></Bottom>
+
+        <Drawer :initDrawerActive="this.drawerActive" :initDrawerPlacement="this.drawerPlacement"
+            @closeDrawer="this.drawerDeactivate" :sketching="false" :inBookShelf="true">
+            <SystemInfo :showSystemInfo="this.drawerActive"    >
+            </SystemInfo>
+        </Drawer>
+
+
     </div>
 </template>
 
@@ -22,12 +33,16 @@
 import Header from "@/components/Header.vue";
 import Bottom from "@/components/Bottom.vue";
 import UploadFile from "@/components/UploadArea.vue";
+import SystemInfo from "@/components/SystemInfo.vue";
+import Drawer from "@/components/Drawer.vue";
 import { defineComponent, reactive } from "vue";
 export default defineComponent({
     name: "UploadPage",
     components: {
         Header, // 自定义页头
         Bottom, // 自定义页尾
+        Drawer, // 自定义抽屉
+        SystemInfo,//自定义系统信息
         UploadFile,//自定义的文件上传领域，一本书也没有的时候用
     },
     setup() {
@@ -59,6 +74,15 @@ export default defineComponent({
         }
     },
     methods: {
+        // 打开抽屉
+        drawerActivate(place) {
+            this.drawerActive = true;
+            this.drawerPlacement = place;
+        },
+        // 关闭抽屉
+        drawerDeactivate() {
+            this.drawerActive = false;
+        },
         getUploadTitile() {
             //如果没有一本书
             if (this.$store.state.server_status.SupportUploadFile === false) {
