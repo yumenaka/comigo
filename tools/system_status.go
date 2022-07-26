87934a306ff0c8d4dc57463cad1da54f48701ebb
yumenaka <www,bailin.tv@gmail.com>
2022-07-26 23:26:22 +0800

    fix init bug && getstatus bug

--- tools/system_status.go
+++ tools/system_status.go
@@ -25,22 +25,50 @@ type SystemStatus struct {
 }
 
 func GetSystemStatus() SystemStatus {
+	sys := SystemStatus{
+		Description:       runtime.GOOS + " " + runtime.GOARCH,
+		CPUNumLogical:     -1,
+		CPUNumPhysical:    -1,
+		CPUUsedPercent:    0,
+		MemoryTotal:       0.0,
+		MemoryFree:        0.0,
+		MemoryUsedPercent: 0,
+	}
 	//获取物理和逻辑核数,以及CPU、内存整体使用率
 	CPUNumLogical, err := cpu.Counts(true)
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		sys.CPUNumLogical = CPUNumLogical
 	}
 	CPUNumPhysical, err := cpu.Counts(false)
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		sys.CPUNumPhysical = CPUNumPhysical
 	}
 	CPUUsedPercent, err := cpu.Percent(0, false)
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		p := 0.0
+		if len(CPUUsedPercent) > 1 {
+			for _, value := range CPUUsedPercent {
+				p += value
+			}
+			p = p / float64(len(CPUUsedPercent))
+		} else if len(CPUUsedPercent) == 1 {
+			p = CPUUsedPercent[0]
+		}
+		sys.CPUUsedPercent = p
 	}
 	v, err := mem.VirtualMemory()
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		sys.MemoryTotal = v.Total
+		sys.MemoryFree = v.Free
+		sys.MemoryUsedPercent = v.UsedPercent
 	}
 	//// almost every return value is a struct
 	//fmt.Printf("Total: %v, Free:%v, UsedPercent:%f%%\n", v.Total, v.Free, v.UsedPercent)
@@ -52,13 +80,5 @@ func GetSystemStatus() SystemStatus {
 	//	fmt.Println(hostname)
 	//}
 
-	return SystemStatus{
-		Description:       runtime.GOOS + " " + runtime.GOARCH,
-		CPUNumLogical:     CPUNumLogical,
-		CPUNumPhysical:    CPUNumPhysical,
-		CPUUsedPercent:    CPUUsedPercent[0],
-		MemoryTotal:       v.Total,
-		MemoryFree:        v.Free,
-		MemoryUsedPercent: v.UsedPercent,
-	}
+	return sys
 }
