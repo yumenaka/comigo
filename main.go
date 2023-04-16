6cdb00e04666409bb56745f8d219dd6bc70a4442
yumenaka <www.bailin.tv@gmail.com>
2023-04-17 00:07:49 +0900

    fix bug

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
-//go:build windows
-// +build windows
-
 //go:generate go install -v github.com/josephspurrier/goversioninfo/cmd/goversioninfo
 //go:generate goversioninfo -icon=icon.ico -manifest=goversioninfo.exe.manifest
+
 package main
 
 import (
