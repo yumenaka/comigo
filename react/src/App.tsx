44db142833937a655cf2e1192c46e26f7db04220
yumenaka <www.bailin.tv@gmail.com>
2023-07-24 21:09:58 +0900

    react config

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,28 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+type Config = {
+  Port: number;
+  Host: string;
+  StoresPath: [];
+  MaxDepth: number;
+  OpenBrowser: boolean;
+  DisableLAN: boolean;
+  DefaultMode: string;
+  UserName: string;
+  Password: string;
+  Timeout: number;
+  CertFile: string;
+  KeyFile: string;
+  CacheEnable: boolean;
+  CachePath: string;
+  CacheClean: boolean;
+  EnableUpload: boolean;
+  UploadPath: string;
+  EnableDatabase: boolean;
+  ClearDatabase: boolean;
+  ExcludeFileOrFolders: [];
+  SupportMediaType: [];
+  SupportFileType: [];
+  MinImageNum: number;
+  TimeoutLimitForScan: number;
+  PrintAllIP: boolean;
+  Debug: boolean;
+  LogToFile: boolean;
+  LogFilePath: string;
+  LogFileName: string;
+  ZipFileTextEncoding: string;
+  EnableFrpcServer: boolean;
+  FrpConfig: {
+    FrpcCommand: string;
+    ServerAddr: string;
+    ServerPort: number;
+    Token: string;
+    FrpType: string;
+    RemotePort: number;
+    RandomRemotePort: boolean;
+  };
+  GenerateMetaData: boolean;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const baseURL = "/api";
+  const [config, setConfig] = useState<Config | null>(null);
+
+  useEffect(() => {
+    axios
+      .get<Config>(`${baseURL}/config.json`)
+      .then((response) => {
+        setConfig(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  console.log(config);
 
   return (
     <>
       <div>
-        <a className="flex flex-row justify-center items-center w-full"  href="https://react.dev" target="_blank">
+        <a
+          className="flex flex-row justify-center items-center w-full"
+          href="https://react.dev"
+          target="_blank"
+        >
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+
+      <div className="card">Now port is {config?.Port}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
