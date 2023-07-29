ca166c616a5e1914f9448f76499b32f16942f1b1
yumenaka <www.bailin.tv@gmail.com>
2023-07-29 12:37:30 +0900

    add types

---
+++ react/src/types/Config.ts
@@ -0,0 +1,45 @@
+type Config = {
+  Port: number;
+  Host: string;
+  StoresPath: [];
+  MaxScanDepth: number;
+  OpenBrowser: boolean;
+  DisableLAN: boolean;
+  DefaultMode: string;
+  UserName: string;
+  Password: string;
+  Timeout: number;
+  CertFile: string;
+  KeyFile: string;
+  EnableLocalCache: boolean;
+  CachePath: string;
+  ClearCacheExit: boolean;
+  EnableUpload: boolean;
+  UploadPath: string;
+  EnableDatabase: boolean;
+  ClearDatabase: boolean;
+  ExcludeFileOrFolders: [];
+  SupportMediaType: [];
+  SupportFileType: [];
+  MinImageNum: number;
+  TimeoutLimitForScan: number;
+  PrintAllIP: boolean;
+  Debug: boolean;
+  LogToFile: boolean;
+  LogFilePath: string;
+  LogFileName: string;
+  ZipFileTextEncoding: string;
+  EnableFrpcServer: boolean;
+  FrpConfig: {
+    FrpcCommand: string;
+    ServerAddr: string;
+    ServerPort: number;
+    Token: string;
+    FrpType: string;
+    RemotePort: number;
+    RandomRemotePort: boolean;
+  };
+  GenerateMetaData: boolean;
+};
+
+export default Config;
