9410dd62e5719df6ab1e4e0501b73cbe65f272df
yumenaka <www.bailin.tv@gmail.com>
2023-09-01 23:08:05 +0900

    react: fix bug.

---
+++ react/src/reducers/configReducer.ts
@@ -0,0 +1,71 @@
+import Config  from "../types/Config";
+
+type Action = {
+    type:
+    | "downloadConfig"
+    | "boolConfig"
+    | "stringConfig"
+    | "numberConfig"
+    | "arrayConfig";
+    name: string;
+    value: boolean | string | number | string[];
+    config: Config;
+};
+
+export function configReducer(c: Config, action: Action) {
+    switch (action.type) {
+        case "downloadConfig":
+            return { ...action.config };
+        case "boolConfig":
+            return { ...c, [action.name]: action.value };
+        case "stringConfig":
+            return { ...c, [action.name]: action.value };
+        case "numberConfig":
+            return { ...c, [action.name]: action.value };
+        case "arrayConfig":
+            return { ...c, [action.name]: action.value };
+        default:
+            console.log(action);
+            throw new Error();
+    }
+}
+
+export const defaultConfig:Config={
+    Port: 8080,
+    Host: "",
+    StoresPath: [],
+    MaxScanDepth: 10,
+    OpenBrowser: true,
+    DisableLAN: false,
+    DefaultMode: "all",
+    Username: "",
+    Password: "",
+    Timeout: 30,
+    CertFile: "",
+    KeyFile: "",
+    EnableLocalCache: true,
+    CachePath: "",
+    ClearCacheExit: false,
+    EnableUpload: false,
+    UploadPath: "",
+    EnableDatabase: false,
+    ClearDatabaseWhenExit: false,
+    ExcludePath: [],
+    SupportMediaType: [],
+    SupportFileType: [],
+    MinImageNum: 2,
+    TimeoutLimitForScan: 10,
+    PrintAllPossibleQRCode: false,
+    Debug: false,
+    LogToFile: false,
+    LogFilePath: "",
+    LogFileName: "",
+    ZipFileTextEncoding: "utf-8",
+    EnableFrpcServer: false,
+    FrpConfig: {
+        FrpcCommand: "",
+        ServerAddr: "",
+        ServerPort: 0,
+    },
+    GenerateMetaData: false,
+}
