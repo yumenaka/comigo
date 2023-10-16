8f97d62bee2e166fe14198e61c6bda72c4bbdd42
yumenaka <www.bailin.tv@gmail.com>
2023-10-16 21:56:52 +0900

    fix windows_386 bug.

--- database/fake_windows_386.go
+++ database/fake_windows_386.go
@@ -6,7 +6,7 @@ import (
 	"errors"
 	"fmt"
 
-	comigoBook "github.com/yumenaka/comi/types"
+	mainTypes "github.com/yumenaka/comi/types"
 )
 
 func InitDatabase(configFilePath string) error {
@@ -20,8 +20,12 @@ func CloseDatabase() {
 
 // DeleteAllBookInDatabase  清空数据库的Book与SinglePageInfo表
 // 后台并发执行，所以不能保证结果如预期，不用这个函数。
-func ClearBookData(clearBook *comigoBook.Book, debug bool) {
+func ClearBookData(clearBook *mainTypes.Book) {
 	fmt.Println("Not Support DateBase")
+}
+
+func GetBooksFromDatabase() (list []*mainTypes.Book, err error) {
+	return nil, errors.New("Not Support DateBase")
 }
 
 // DeleteAllBookInDatabase  清空数据库的Book与SinglePageInfo表
@@ -31,27 +35,27 @@ func DeleteAllBookInDatabase(debug bool) {
 }
 
 // SaveAllBookToDatabase 将Map里面的书籍信息，全部保存到本地数据库中
-func SaveAllBookToDatabase(databaseFilePath string, m map[string]*comigoBook.Book) {
+func SaveAllBookToDatabase(databaseFilePath string, m map[string]*mainTypes.Book) {
 	fmt.Println("Not Support DateBase")
 }
 
 // SaveBookListToDatabase  向数据库中插入一组书
-func SaveBookListToDatabase(bookList []*comigoBook.Book) error {
+func SaveBookListToDatabase(bookList []*mainTypes.Book) error {
 	return errors.New("Not Support DateBase")
 }
 
 // SaveBookToDatabase 向数据库中插入一本书
-func SaveBookToDatabase(save *comigoBook.Book) error {
+func SaveBookToDatabase(save *mainTypes.Book) error {
 	return errors.New("Not Support DateBase")
 }
 
 // GetBookFromDatabase 根据文件路径，从数据库查询一本书的详细信息,避免重复扫描压缩包。
-func GetBookFromDatabase(filepath string) (*comigoBook.Book, error) {
+func GetBookFromDatabase(filepath string) (*mainTypes.Book, error) {
 	return nil, errors.New("Not Support DateBase")
 }
 
 // GetBooksFromDatabase  根据文件路径，从数据库查询书的详细信息,避免重复扫描压缩包。//忽略文件夹型的书籍
-func GetArchiveBookFromDatabase() (list []*comigoBook.Book, err error) {
+func GetArchiveBookFromDatabase() (list []*mainTypes.Book, err error) {
 	return nil, errors.New("Not Support DateBase")
 }
 
@@ -69,7 +73,7 @@ func CleanDatabaseByLocalData() {
 //
 
 //// CleanAndSaveAllBookToDatabase  同时清空Map里面不存在的书。然后将Map里面的书籍信息，全部保存到本地数据库中。
-//func CleanAndSaveAllBookToDatabase(databaseFilePath string, m map[string]*comigoBook.Book) error {
+//func CleanAndSaveAllBookToDatabase(databaseFilePath string, m map[string]*mainTypes.Book) error {
 //	//Open函数默认创建的db文件权限是0600(属主读写),1秒超时的选项,可以自行传值修改
 //	//db, err := storm.Open(path.Join(databaseFilePath, "comigo.db"), storm.BoltOptions(0600, &bolt.Options{Timeout: 100 * time.Millisecond}))
 //	//if err != nil {
