af52677b3967fbee6f553bfab79bce18f2738a74
yumenaka <www.bailin.tv@gmail.com>
2023-11-10 22:36:38 +0900

    QuickJumpBar:fix bug

--- web/src/components/QuickJumpBar.vue
+++ web/src/components/QuickJumpBar.vue
@@ -1,64 +1,73 @@
 <template>
-  <div class="w-full my-3 flex flex-row justify-center content-center">
-    <div class=" text-gray-900 h-12 py-4 self-center text-base">
-      <a href="" class="text-blue-700 hover:underline text-3xl font-semibold">⬅️</a>
-    </div>
-    <select class="mx-2 p-2 w-1/2 border-gray-200 rounded-lg text-xl font-semibold
-     text-center disabled:opacity-50 disabled:pointer-events-none"
+  <div class="w-full my-3 flex flex-row justify-center items-center">
+    <a :href="prevLink" class="text-blue-700 hover:underline text-4xl font-semibold">⬅️</a>
+    <select
+      class="mx-2 p-2 w-1/2 border-gray-200 rounded-lg text-xl font-semibold text-center disabled:opacity-50 disabled:pointer-events-none"
       onchange="location = '/#/scroll/'+this.value;location.reload();">
       <option v-for="book in quick_jump_info.BaseBooks" :value="book.id" :key="book.id" :selected="book.id == nowBookID">
-        {{ book.name}}
+        {{ book.name }}
       </option>
     </select>
-    <div class=" text-gray-900 h-12 py-4 space-x-2 text-base content-center">
-      <a href="" class="text-blue-700 hover:underline text-3xl font-semibold">➡️</a>
-    </div>
+    <a :href="nextLink" class="text-blue-700 hover:underline text-4xl font-semibold ">➡️</a>
   </div>
 </template>
 
-<script lang ="ts">
-import { defineComponent } from 'vue'
-import axios from "axios";
+<script lang="ts">
+import { defineComponent } from 'vue';
+import axios from 'axios';
+
 export default defineComponent({
   name: 'QuickJumpBar',
   props: ['nowBookID'],
   data() {
     return {
-      SomeFlag: "",
+      SomeFlag: '',
       quick_jump_info: {
         BaseBooks: [
           {
             id: 0,
-            name: "",
-          }
-        ]
+            name: '',
+          },
+        ],
       },
+      selectedBook: "",
     };
   },
+  computed: {
+    prevLink() {
+      for (let i = 0; i < this.quick_jump_info.BaseBooks.length; i++) {
+        if (this.quick_jump_info.BaseBooks[i].id === this.nowBookID) {
+          if (i === 0) {
+            return `/#/scroll/${this.nowBookID}`;
+          }
+          return `/#/scroll/${this.quick_jump_info.BaseBooks[i - 1].id}`;
+        }
+      }
+    },
+    nextLink() {
+      for (let i = 0; i < this.quick_jump_info.BaseBooks.length; i++) {
+        if (this.quick_jump_info.BaseBooks[i].id === this.nowBookID) {
+          if (i === this.quick_jump_info.BaseBooks.length - 1) {
+            return `/#/scroll/${this.nowBookID}`;
+          }
+          return `/#/scroll/${this.quick_jump_info.BaseBooks[i + 1].id}`;
+        }
+      }
+    },
+  },
   created() {
-    axios
-      .get("/quick_jump_info?id=" + this.$route.params.id)
-      .then((response) => {
-        //请求接口成功的逻辑
-        this.quick_jump_info = response.data;
-        console.log(this.quick_jump_info);
-        console.log(this.quick_jump_info.BaseBooks);
-        console.log(this.quick_jump_info.BaseBooks.length);
-        //debugger
-      }).catch((error: any) => {
-        console.log(error);
-      });
+    this.selectedBook = this.nowBookID;
+    this.fetchQuickJumpInfo();
   },
   methods: {
-    //选择书籍的时候，跳转到对应章节
-    onQuickJump() {
-      if (this.nowBookID == 0) {
-        return;
+    async fetchQuickJumpInfo() {
+      try {
+        const response = await axios.get(`/quick_jump_info?id=${this.$route.params.id}`);
+        this.quick_jump_info = response.data;
+      } catch (error) {
+        console.log(error);
       }
-      console.log("onQuickJump");
-      window.location.href = '/#/scroll/' + this.nowBookID;
-      window.location.reload();
-    },
+    }
   },
 });
 </script>
