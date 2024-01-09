dc027b06a1903b5aa067741f081cc90bb4105ceb
yumenaka <www.bailin.tv@gmail.com>
2024-01-09 23:47:28 +0900

    flutter:json api.

--- mobile_app/lib/models/book.dart
+++ mobile_app/lib/models/book.dart
@@ -1,5 +1,24 @@
 import 'image_info.dart';
 
+// 在Flutter中发起HTTP网络请求 https://doc.flutterchina.club/networking/
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<List<Book>> fetchBooks() async {
+  final response = await http.get(Uri.parse('http://192.168.3.8:1234/api/book_infos?depth=0&sort_by=name'));
+
+  if (response.statusCode == 200) {
+    List<Book> bookshelf = [];
+    List<dynamic> booksJson = jsonDecode(response.body);
+    for (var bookJson in booksJson) {
+      bookshelf.add(Book.fromJson(bookJson));
+    }
+    return bookshelf;
+  } else {
+    throw Exception('Failed to load books');
+  }
+}
+
 class Book {
   String title;
   String id;
