b3d895cb94c0121186bdfbe716e91794a46281d5
yumenaka <www.bailin.tv@gmail.com>
2024-01-24 21:47:10 +0900

    flutter:尝试用FutureBuilder做异步UI更新

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'models/book.dart';
 
 void main() {
@@ -12,33 +11,23 @@ class ComigoApp extends StatelessWidget {
   // 这个小部件是应用的根部件。
   @override
   Widget build(BuildContext context) {
-
     // 谷歌推荐的Material（ Android 默认的视觉风格）的组件库
     return MaterialApp(
       title: 'Comigo Demo v1.0',
-      //debug条件下，显示右上角的debug标签。我不需要，所以设置为false。
+      //隐藏右上角的debug标签
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // 这是应用程序的主题。
-        //
-        // 尝试一下：运行应用程序，使用 "flutter run" 命令。会看到应用程序有一个蓝色的工具栏。
-        // 然后，在不退出应用程序的情况下，尝试将 colorScheme 中的 seedColor 更改为 Colors.green，
-        // 然后触发 "hot reload"（保存更改或在支持Flutter的IDE中按下 "hot reload" 按钮，或者如果你使用命令行启动应用程序，则按下 "r"）。
-        //
-        // 注意，计数器没有重置为零；应用程序状态在重新加载期间不会丢失。要重置状态，请使用热重启。
-        //
-        // 这对于代码也适用，不仅仅是值：大多数代码更改只需要进行热重载即可测试。
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
-      home: const ComigoHomePage(title: 'Comigo Home Page'),
+      home: const ComigoHomePage(title: 'Comigo v1.0'),
     );
   }
 }
 
 class ComigoHomePage extends StatefulWidget {
   const ComigoHomePage({super.key, required this.title});
-
   // 这个小部件是应用的主页。它是有状态的，意味着它有一个包含影响其外观的字段的 State 对象（在下面定义）。
   // 这个类是状态的配置。它保存了父级（在这种情况下是 App 小部件）提供的值（在这种情况下是标题），并由状态的 build 方法使用。
   // Widget 子类中的字段始终标记为 "final"。
@@ -49,78 +38,71 @@ class ComigoHomePage extends StatefulWidget {
 }
 
 class _ComigoHomePageState extends State<ComigoHomePage> {
-  int _counter = 0;
 
-  Future<List<Book>>? booksFuture;
+
+  Future<List<Book>>? Books;
   @override
   void initState() {
     super.initState();
-    booksFuture = fetchBooks(); // 调用函数并初始化参数
-    booksFuture?.then((value) => {
+    Books = fetchBooks(); // 调用函数并初始化参数
+    Books?.then((value) => {
       for (var book in value) {
-        print(book.title)
+        debugPrint(book.title)
       }
     });
   }
 
-  void _incrementCounter() {
-    setState(() {
-      // 这个调用setState告诉Flutter框架，这个状态发生了变化，
-      // 导致重新运行下面的build方法，以便显示可以反映更新后的值。
-      // 如果我们在不调用setState的情况下更改_counter，
-      // 那么build方法将不会被再次调用，因此不会有任何变化显示出来。
-      _counter = _counter + 2;
-    });
-  }
-
-  void _minusCounter() {
-    setState(() {
-      _counter = _counter - 2;
-    });
+  Future<List<Book>> initBooks() async {
+    Future<List<Book>>? books = fetchBooks(); // 调用函数并初始化参数
+    return books.then((value) => value);
   }
 
   @override
   Widget build(BuildContext context) {
+    //  画面の高さを取得する
+    final mediaQueryData = MediaQuery.of(context);
+    // トップナビゲーションバーの高さ
+    final headerHeight = mediaQueryData.size.height * 0.07;
+    // ボトムナビゲーションバーの高さ
+    // final bottomHeight = mediaQueryData.size.height * 0.1;
+    // 异步UI更新（FutureBuilder、StreamBuilder）
+    // https://book.flutterchina.club/chapter7/futurebuilder_and_streambuilder.html
+    Widget booksWidget = FutureBuilder<List<Book>>(
+      future: fetchBooks(),
+      initialData: [Book(title: 'Loading...', id: '0', type: '0')],
+      // snapshot会包含当前异步任务的状态信息及结果信息
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          debugPrint(snapshot.data!.length.toString());
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(snapshot.data![index].title),
+                subtitle: Text(snapshot.data![index].id),
+              );
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+        return const CircularProgressIndicator();
+      },
+    );
+
     // 每次调用setState时，此方法都会重新运行，例如上面的_incrementCounter方法。
-    //
     // Flutter框架已经进行了优化，使重新运行build方法变得快速，因此您只需重新构建需要更新的内容，
     // 而不必逐个更改小部件的实例。
     return Scaffold(
-      backgroundColor: Colors.yellowAccent,
+      backgroundColor: Colors.yellow[200],
       appBar: AppBar(
-        // 试试这个：尝试在这里将颜色更改为特定的颜色（例如Colors.amber），然后触发热重载，看看AppBar的颜色是否改变，而其他颜色保持不变。
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // 在这里，我们从由App.build方法创建的MyHomePage对象中获取值，并将其用于设置我们的应用栏标题。
-        title: Text(widget.title),
+        title: Text(widget.title,
+            style: Theme.of(context).textTheme.headlineMedium),
+        backgroundColor: Colors.lightBlue, ////appbarの背景色を設定する
+        toolbarHeight: headerHeight, //appbarの高さを設定する
+        centerTitle: true, //タイトルを中央に配置
       ),
-      body: Center(
-        // Center是一个布局小部件。它接受一个子部件并将其放置在父级的中间位置。
-        child: Row(
-          // Column也是一个布局小部件。它接受一个子部件列表并垂直排列它们。默认情况下，它会水平调整自身大小以适应其子部件，并尽量与其父级一样高。
-          // Column有各种属性来控制其自身的大小和子部件的位置。在这里，我们使用mainAxisAlignment来垂直居中子部件；主轴在这里是垂直轴，因为Column是垂直的（交叉轴将是水平的）。
-          // 试试这个：调用“调试绘制”（在IDE中选择“切换调试绘制”操作，或在控制台中按“p”），以查看每个小部件的线框。
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              '你已经点击按钮：',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            FloatingActionButton.extended(
-              onPressed: _minusCounter,
-              label: const Text('Minus'),
-              icon: const Icon(Icons.article),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // 这个尾随逗号使构建方法的自动格式化更加美观。
+      body: booksWidget,
     );
   }
 }
