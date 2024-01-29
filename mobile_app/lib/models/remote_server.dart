2ce9e843e8a8b26c535454ec8f075d1e62f70bc5
yumenaka <www.bailin.tv@gmail.com>
2024-01-29 23:54:51 +0900

    flutter:setting

--- mobile_app/lib/models/remote_server.dart
+++ mobile_app/lib/models/remote_server.dart
@@ -5,10 +5,10 @@ import 'package:shared_preferences/shared_preferences.dart';
 class RemoteServer extends ChangeNotifier {
   String remoteHost = "";
   RemoteServer() {
-    initHost();
+    init();
   }
 
-  Future<void> initHost() async {
+  Future<void> init() async {
     final prefs = await SharedPreferences.getInstance();
     remoteHost = prefs.getString('comigo_host') ?? "http://127.0.0.1:1234";
     //模型发生改变并且需要更新 UI 的时候调用该方法
