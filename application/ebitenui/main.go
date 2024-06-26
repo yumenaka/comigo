b880aea1366a9c0d079ac5b7f43ce86076debd42
yumenaka <www.bailin.tv@gmail.com>
2024-06-26 21:24:23 +0900

    EbitenUI: Add Pages.

--- application/ebitenui/main.go
+++ application/ebitenui/main.go
@@ -3,20 +3,21 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/ebitenui/ebitenui"
-	"github.com/ebitenui/ebitenui/image"
-	"github.com/ebitenui/ebitenui/widget"
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"github.com/yumenaka/comi/application/ebitenui/components"
-	"github.com/yumenaka/comi/application/ebitenui/model"
-	_ "golang.org/x/image/bmp"
-	_ "golang.org/x/image/webp"
-	"image/color"
+	"github.com/yumenaka/comi/application/ebitenui/pages"
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
 	"log"
+
+	"github.com/ebitenui/ebitenui"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/yumenaka/comi/application/ebitenui/comi"
+	"github.com/yumenaka/comi/application/ebitenui/components"
+	"github.com/yumenaka/comi/application/ebitenui/model"
+	"github.com/yumenaka/comi/config"
+	_ "golang.org/x/image/bmp"
+	_ "golang.org/x/image/webp"
 )
 
 // Game object used by ebiten
@@ -36,11 +37,14 @@ func main() {
 	}
 	// 启动Comigo Web服务器
 	// 如果参数当中有--debug，则不启动UI，且什么都不做（UI抢焦点，所以用另一台机器rsync同步代码跑效果）
-	StartComigoWebserver()
+	comi.StartComigoWebserver()
 
 	// 创建一个新的 ReaderConfig 对象，用于配置阅读器的设置。
 	readerConfig := model.NewReaderConfig()
-	readerConfig.SetTitle("Comigo v0.9").
+	config.Config.OpenBrowser = false
+	config.Config.UseCache = true
+	config.Config.ClearCacheExit = false
+	readerConfig.SetTitle("Comigo "+config.Version).
 		// 阅读器模式。
 		SetReaderMode(model.ScrollMode).
 		// 窗口是否全屏。
@@ -70,7 +74,7 @@ func main() {
 	// 默认值为 true，这意味着默认情况下每个帧都会清屏。
 	ebiten.SetScreenClearedEveryFrame(false)
 
-	// 创建UI
+	// 构建UI
 	eui, closeUI, err := createUI(readerConfig)
 	if err != nil {
 		log.Fatal(err)
@@ -93,34 +97,8 @@ func main() {
 
 // 构建UI，需要进一步拆分
 func createUI(readerConfig *model.ReaderConfig) (*ebitenui.UI, func(), error) {
-	// 2.根容器与布局设置，
-	//参考了 https://github.com/ebitenui/ebitenui/tree/master/_examples/widget_demos/gridlayout/main.go
-	// 为此 UI 创建根容器。所有其他 UI 元素都必须添加到此容器中。
-	// 在 Ebiten UI 中，小部件通常不是手动布局的。相反，它们作为子部件被组合在一个容器中，容器的布局器负责布局这些部件。
-	rootContainer :=
-		widget.NewContainer(
-			//使用纯色作为背景
-			widget.ContainerOpts.BackgroundImage(image.NewNineSliceColor(color.NRGBA{A: 255})),
-			// 容器将使用锚布局来布局其单个子窗口小部件
-			widget.ContainerOpts.Layout(
-				//GridLayout 可以在网格中布置任意数量的小部件。它可以为每个网格单元的小部件设置不同的位置，并且还可以拉伸它们。
-				// 根据小部件的实现方式，某些选项是必须指定的（例如按钮的图片），而其他选项则是可选的。选项的顺序通常无关紧要。有些选项可以被多次指定。
-				widget.NewGridLayout(
-					// 使用 Columns 参数来定义列的数量。
-					widget.GridLayoutOpts.Columns(1),
-					// 使用 ColumnStretch 和 RowStretch 参数来分别定义列和行的拉伸因子。
-					// 只支持布尔值，true表示拉伸，false表示不拉伸。
-					widget.GridLayoutOpts.Stretch([]bool{true}, []bool{false, true, false}),
-					//网格布局的间距，c 列间距，r行间距。
-					widget.GridLayoutOpts.Spacing(20, 0),
-					// Padding 定义了网格块的外间距大小。
-					widget.GridLayoutOpts.Padding(widget.Insets{
-						Top:    0,
-						Left:   0,
-						Bottom: 0,
-						Right:  0,
-					}),
-				)))
+
+	rootContainer := pages.BookShelf()
 
 	//这会将根容器添加到 UI，以便将其展示。
 	eui := &ebitenui.UI{
---
+++ application/ebitenui/pages/flip.go
@@ -0,0 +1,39 @@
+package pages
+
+import (
+	"github.com/ebitenui/ebitenui/image"
+	"github.com/ebitenui/ebitenui/widget"
+	"image/color"
+)
+
+// FlipMode  滚动模式
+func FlipMode() *widget.Container {
+	// 2.根容器与布局设置，
+	//参考了 https://github.com/ebitenui/ebitenui/tree/master/_examples/widget_demos/gridlayout/main.go
+	// 为此 UI 创建根容器。所有其他 UI 元素都必须添加到此容器中。
+	// 在 Ebiten UI 中，小部件通常不是手动布局的。相反，它们作为子部件被组合在一个容器中，容器的布局器负责布局这些部件。
+	FlipMode := widget.NewContainer(
+		//使用纯色作为背景
+		widget.ContainerOpts.BackgroundImage(image.NewNineSliceColor(color.NRGBA{A: 255})),
+		// 容器将使用锚布局来布局其单个子窗口小部件
+		widget.ContainerOpts.Layout(
+			//GridLayout 可以在网格中布置任意数量的小部件。它可以为每个网格单元的小部件设置不同的位置，并且还可以拉伸它们。
+			// 根据小部件的实现方式，某些选项是必须指定的（例如按钮的图片），而其他选项则是可选的。选项的顺序通常无关紧要。有些选项可以被多次指定。
+			widget.NewGridLayout(
+				// 使用 Columns 参数来定义列的数量。
+				widget.GridLayoutOpts.Columns(1),
+				// 使用 ColumnStretch 和 RowStretch 参数来分别定义列和行的拉伸因子。
+				// 只支持布尔值，true表示拉伸，false表示不拉伸。
+				widget.GridLayoutOpts.Stretch([]bool{true}, []bool{false, true, false}),
+				//网格布局的间距，c 列间距，r行间距。
+				widget.GridLayoutOpts.Spacing(20, 0),
+				// Padding 定义了网格块的外间距大小。
+				widget.GridLayoutOpts.Padding(widget.Insets{
+					Top:    0,
+					Left:   0,
+					Bottom: 0,
+					Right:  0,
+				}),
+			)))
+	return FlipMode
+}
