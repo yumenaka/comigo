0fe28c40ffc9762206ff12f715a7f056cfa775ba
yumenaka <www.bailin.tv@gmail.com>
2024-08-02 19:11:30 +0900

    htmx: add admin page(react)

--- htmx/templates/pages/shelf_templ.go
+++ htmx/templates/pages/shelf_templ.go
@@ -48,7 +48,7 @@ func ShelfPage(s *state.GlobalState) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = components.Footer("Comigo "+" "+s.Version).Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = components.Footer(s.Version).Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
