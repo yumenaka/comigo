928bf01d80f1d6098ce80d1a7cae1526b2867869
yumenaka <www.bailin.tv@gmail.com>
2024-08-03 12:53:13 +0900

    htmx: add Alpine Plugin(Persist)

--- htmx/router/assets/scripts.js
+++ htmx/router/assets/scripts.js
@@ -1,9 +1,14 @@
 import 'htmx.org'
-
 import Alpine from 'alpinejs'
+import persist from '@alpinejs/persist'
 
 // Add Alpine instance to window object.
 window.Alpine = Alpine
+
+// Alpine Persist 插件，用于持久化存储。默认存储到 localStorage。
+// 详细用法参见： https://alpinejs.dev/plugins/persist
+Alpine.plugin(persist)
+
 
 // Start Alpine.
 Alpine.start()
