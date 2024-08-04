eb12e5763fd77b6430428340225a25903b3505e9
yumenaka <www.bailin.tv@gmail.com>
2024-08-04 23:48:01 +0900

    htmx: fix theme bug

--- htmx/templates/pages/scroll_templ.go
+++ htmx/templates/pages/scroll_templ.go
@@ -95,7 +95,7 @@ func ScrollPage(s *state.GlobalState) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div><div class=\"drawer-side\"><label for=\"my-drawer\" aria-label=\"close sidebar\" class=\"drawer-overlay\"></label><ul class=\"min-h-full p-4 menu bg-base-200 text-base-content w-80\"><!-- Sidebar content here --><li><a>Sidebar Item 1</a></li><li><a>Sidebar Item 2</a></li></ul></div></div>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div><div class=\"drawer-side\"><label for=\"my-drawer\" aria-label=\"close sidebar\" class=\"drawer-overlay\"></label><ul class=\"min-h-full p-4 menu bg-base-100 text-base-content w-80\"><!-- Sidebar content here --><li><a>Sidebar Item 1</a></li><li><a>Sidebar Item 2</a></li></ul></div></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
