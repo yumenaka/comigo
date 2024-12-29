b68f8c8b3c2c70af1bab4280434454571b87a2e6
yumenaka <www.bailin.tv@gmail.com>
2024-12-29 22:29:26 +0900

    move file

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/yumenaka/comigo/cmd/tui"
+	"golang.org/x/term"
 	"os"
 	"path"
 	"path/filepath"
@@ -20,7 +23,32 @@ var rootCmd = &cobra.Command{
 	Example: locale.GetString("comigo_example"),
 	Version: config.GetVersion(),
 	Long:    locale.GetString("long_description"),
-	// 不加参数的命令。
+	// Run 函数按以下顺序执行：
+	// PersistentPreRun() PreRun() Run() PostRun() PersistentPostRun()
+	// 所有函数都获得相同的参数，即命令名称后面的参数。
+	// 仅当声明了当前命令的 Run 函数时，才会执行 PreRun 和 PostRun 函数。
+	PreRun: func(cmd *cobra.Command, args []string) {
+		//当前 stdout 连接到真实终端时，启动 TUI 界面
+		if term.IsTerminal(int(os.Stdout.Fd())) {
+			// 1. 初始化自定义的日志缓冲区
+			logBuffer := tui.NewLogBuffer()
+			// 将标准日志的输出重定向到 logBuffer
+			logger.SetOutput(logBuffer)
+
+			// 2. 创建 Bubble Tea 程序
+			m := tui.InitialModel(logBuffer)
+			p := tea.NewProgram(m)
+
+			// 3. 运行 TUI 程序
+			go func() {
+				if _, err := p.Run(); err != nil {
+					logger.Errorf("Error running tui interface: %v", err)
+				}
+			}()
+		}
+		//当前 stdout 非终端（可能是文件、管道或者重定向）时，不启动 TUI 界面
+	},
+	// 实际运行的命令大多写在这里
 	Run: func(cmd *cobra.Command, args []string) {
 		//解析命令，扫描文件
 		StartScan(args)
@@ -35,8 +63,8 @@ var rootCmd = &cobra.Command{
 	},
 }
 
-// ReadConfigFile 读取顺序：RAM（代码当中设定的默认值）+命令行参数  -> HomeDirectory -> ProgramDirectory -> WorkingDirectory
-func ReadConfigFile() {
+// LoadConfigFile 读取顺序：RAM（代码当中设定的默认值）+命令行参数  -> HomeDirectory -> ProgramDirectory -> WorkingDirectory
+func LoadConfigFile() {
 	home, err := os.UserHomeDir()
 	if err != nil {
 		logger.Infof("%s", err)
@@ -103,7 +131,7 @@ func Execute() {
 	//初始化命令行参数。不能放在初始化配置文件之后。
 	InitFlags()
 	//初始化配置文件
-	cobra.OnInitialize(ReadConfigFile) // "OnInitialize"传入的函数，应该会在所有命令执行之前，包括rootCmd.Run之前执行。
+	cobra.OnInitialize(LoadConfigFile) // "OnInitialize"传入的函数，应该会在所有命令执行之前，包括rootCmd.Run之前执行。
 	//执行命令
 	if err := rootCmd.Execute(); err != nil {
 		logger.Infof("%s", err)
