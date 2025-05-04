57dc3b8813ad7bae19f5f237d249f6242d079f98
yumenaka <www.bailin.tv@gmail.com>
2025-05-04 22:55:48 +0900

    完善404页面

--- templ/pages/scroll/scroll.go
+++ templ/pages/scroll/scroll.go
@@ -27,21 +27,22 @@ func Handler(c echo.Context) error {
 	}
 	// 读取url参数，获取书籍ID
 	bookID := c.Param("id")
-	// 没有找到书籍，显示 HTTP 404 错误
-	indexTemplate := common.Html(
-		c,
-		&state.Global,
-		error_page.NotFound404(&state.Global),
-		[]string{},
-	)
 	book, err := model.GetBookByID(bookID, sortBy)
 	if err != nil {
 		logger.Infof("GetBookByID: %v", err)
-		// 渲染页面
-		if err := htmx.NewResponse().RenderTempl(c.Request().Context(), c.Response().Writer, indexTemplate); err != nil {
+		// 没有找到书，显示 HTTP 404 错误
+		indexHtml := common.Html(
+			c,
+			&state.Global,
+			error_page.NotFound404(&state.Global),
+			[]string{},
+		)
+		// 渲染 404 页面
+		if err := htmx.NewResponse().RenderTempl(c.Request().Context(), c.Response().Writer, indexHtml); err != nil {
 			// 渲染失败，返回 HTTP 500 错误。
 			return c.NoContent(http.StatusInternalServerError)
 		}
+		return nil
 	}
 	// 读取分页索引
 	paginationIndex := -1
@@ -55,14 +56,14 @@ func Handler(c echo.Context) error {
 	// 定义模板主体内容。
 	scrollPage := ScrollPage(&state.Global, book, paginationIndex)
 	// 拼接页面
-	indexTemplate = common.Html(
+	indexHtml := common.Html(
 		c,
 		&state.Global,
 		scrollPage, // define body content
 		[]string{"script/scroll.js"},
 	)
 	// 渲染页面
-	if err := htmx.NewResponse().RenderTempl(c.Request().Context(), c.Response().Writer, indexTemplate); err != nil {
+	if err := htmx.NewResponse().RenderTempl(c.Request().Context(), c.Response().Writer, indexHtml); err != nil {
 		// 渲染失败，返回 HTTP 500 错误。
 		return c.NoContent(http.StatusInternalServerError)
 	}
--- templ/pages/upload_page/upload.go
+++ templ/pages/upload_page/upload.go
@@ -11,14 +11,14 @@ import (
 
 // Handler 上传文件页面
 func Handler(c echo.Context) error {
-	indexTemplate := common.Html(
+	indexHtml := common.Html(
 		c,
 		&state.Global,
 		UploadPage(&state.Global),
 		[]string{},
 	)
 	// 渲染页面
-	if err := htmx.NewResponse().RenderTempl(c.Request().Context(), c.Response().Writer, indexTemplate); err != nil {
+	if err := htmx.NewResponse().RenderTempl(c.Request().Context(), c.Response().Writer, indexHtml); err != nil {
 		// 渲染失败，返回 HTTP 500 错误。
 		return c.NoContent(http.StatusInternalServerError)
 	}
