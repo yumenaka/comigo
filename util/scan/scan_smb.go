0028d72f1055706bd5cbca18dde6486aa9a5e00d
yumenaka <www.bailin.tv@gmail.com>
2025-05-06 23:29:11 +0900

    fix log

--- util/scan/scan_smb.go
+++ util/scan/scan_smb.go
@@ -127,7 +127,7 @@ func Smb(scanOption Option) (newBookList []*model.Book, err error) {
 		})
 	// 所有可用书籍，包括压缩包与文件夹
 	if len(newBookList) > 0 {
-		logger.Infof(locale.GetString("found_in_path"), len(newBookList), scanOption.Cfg.GetStores()[0].Smb.ShareName)
+		logger.Infof(locale.GetString("found_in_path"), scanOption.Cfg.GetStores()[0].Smb.ShareName, len(newBookList))
 		return newBookList, err
 	}
 	return nil, errors.New("NO_BOOKS_FOUND in SMB:" + scanOption.Cfg.GetStores()[0].Smb.ShareName)
