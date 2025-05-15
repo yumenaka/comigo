15af92478e6966ba75d85cb4ac6d61ecbe67000b
yumenaka <www.bailin.tv@gmail.com>
2025-05-16 00:50:58 +0900

    改进静态文件模式

--- main.go
+++ main.go
@@ -4,8 +4,14 @@
 package main
 
 import (
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/x/term"
 	"github.com/yumenaka/comigo/cmd"
+	"github.com/yumenaka/comigo/cmd/tui"
 	"github.com/yumenaka/comigo/routers"
+	"github.com/yumenaka/comigo/util/logger"
 )
 
 // 运行 Comigo 服务器
@@ -20,23 +26,45 @@ func main() {
 	cmd.ShowQRCode()
 	// 退出时清理临时文件的处理函数
 	cmd.SetShutdownHandler()
+
+	// RunTui()
 }
 
-// // tui实验（TODO）
-// func RunTui() {
-//	if term.IsTerminal(os.Stdout.Fd()) {
-//		// 1. 初始化自定义的日志缓冲区
-//		logBuffer := tui.NewLogBuffer()
-//		// 将标准日志的输出重定向到 logBuffer
-//		logger.SetOutput(logBuffer)
-//
-//		// 2. 创建 Bubble Tea 程序
-//		m := tui.InitialModel(logBuffer)
-//		p := tea.NewProgram(m)
-//
-//		// 3. 运行 TUI 程序
-//		if _, err := p.Run(); err != nil {
-//			logger.Errorf("Error running tui interface: %v", err)
-//		}
-//	}
-// }
+// RunTui tui实验
+func RunTui() {
+	// 判断是否在终端中运行
+	if term.IsTerminal(os.Stdout.Fd()) {
+		// 1. 初始化自定义的日志缓冲区
+		logBuffer := tui.NewLogBuffer()
+		// 将标准日志的输出重定向到 logBuffer
+		logger.SetOutput(logBuffer)
+
+		// 2. 创建 Bubble Tea 程序的模型
+		model := tui.InitialModel(logBuffer)
+		// 创建一个bubbletea的应用对象
+		program := tea.NewProgram(model)
+
+		// Comigo 服务器的初始化(初始化 Comigo 命令行flag与args，环境变量与配置文件)
+		cmd.Execute()
+		// 启动网页服务器（不阻塞）
+		routers.StartWebServer()
+		// 扫描书库（命令行指定）
+		cmd.ScanStore(cmd.Args)
+
+		// 3. 调用 Bubble Tea 对象的Start()方法开始执行，运行 TUI 程序
+		if _, err := program.Run(); err != nil {
+			logger.Errorf("Error running tui interface: %v", err)
+		}
+	} else {
+		// 初始化命令行flag与args，环境变量与配置文件
+		cmd.Execute()
+		// 启动网页服务器（不阻塞）
+		routers.StartWebServer()
+		// 扫描书库（命令行指定）
+		cmd.ScanStore(cmd.Args)
+		// 在命令行显示QRCode
+		cmd.ShowQRCode()
+		// 退出时清理临时文件的处理函数
+		cmd.SetShutdownHandler()
+	}
+}
--- routers/get_data_api/get_qrcode.go
+++ routers/get_data_api/get_qrcode.go
@@ -1,6 +1,7 @@
 package get_data_api
 
 import (
+	"encoding/base64"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -13,11 +14,17 @@ import (
 func GetQrcode(c echo.Context) error {
 	// 通过参数传递自定义文本，生成二维码
 	qrcodeStr := c.QueryParam("qrcode_str")
+	base64Encode := getBoolQueryParam(c, "base64", false)
 	if qrcodeStr != "" {
 		png, err := qrcode.Encode(qrcodeStr, qrcode.Medium, 256)
 		if err != nil {
 			logger.Infof("%s", err)
 			return err
+		}
+		if base64Encode {
+			// 返回Base64编码的二维码
+			base64Str := "data:image/png;base64," + base64.StdEncoding.EncodeToString(png)
+			return c.String(http.StatusOK, base64Str)
 		}
 		return c.Blob(http.StatusOK, "image/png", png)
 	}
@@ -27,6 +34,11 @@ func GetQrcode(c echo.Context) error {
 	if err != nil {
 		logger.Infof("%s", err)
 		return err
+	}
+	if base64Encode {
+		// 返回Base64编码的二维码
+		base64Str := "data:image/png;base64," + base64.StdEncoding.EncodeToString(png)
+		return c.String(http.StatusOK, base64Str)
 	}
 	return c.Blob(http.StatusOK, "image/png", png)
 }
