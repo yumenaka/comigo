8ef6cd3fa7eb0cce25a93ee2edc047af52701b71
yumenaka <www.bailin.tv@gmail.com>
2025-10-18 23:22:02 +0900

    优化翻页逻辑：同一浏览器的不同tab之间也支持同步翻页。

--- model/store_group.go
+++ model/store_group.go
@@ -18,17 +18,6 @@ type StoreGroup struct {
 	ChildStores sync.Map // key为路径 存储 *Store
 }
 
-// MainStoreGroup 扫描后生成。可以有多个子书库。内部使用并发安全的 sync.Map 存储书籍和书组
-var MainStoreGroup = StoreGroup{
-	StoreInfo: StoreInfo{
-		BackendURL:  "comigo://main", // 主书库的 URL
-		Name:        "Comigo StoreInfo",
-		Description: "Comigo Main book store",
-	},
-	// 使用 sync.Map 存储书籍和子书库
-	ChildStores: sync.Map{}, // 子书库，储存层级关系
-}
-
 // AddStore 创建一个新书库
 func (storeGroup *StoreGroup) AddStore(storeURL string) error {
 	if _, ok := storeGroup.ChildStores.Load(storeURL); ok {
