4adf4c708e7d1bfe5b73fd5601e8976742b29670
yumenaka <www.bailin.tv@gmail.com>
2025-11-10 22:10:37 +0900

    细节优化： 扫描器：忽略 . 开头的隐藏文件 命令行：打印帮助版本信息后退出 网页端：自动隐藏工具条，默认false 网页端：翻页模式自动跳转后删除start查询参数 网页端：解决网速较慢时，书架与设定页面的闪烁问题

--- cmd/comi/main.go
+++ cmd/comi/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"os"
+
 	"github.com/yumenaka/comigo/cmd"
 	"github.com/yumenaka/comigo/routers"
 )
 
 func main() {
-	// 初始化命令行flag，环境变量与配置文件
+	// 检查是否只是查看版本或帮助信息
+	for _, arg := range os.Args {
+		if arg == "-v" || arg == "--version" || arg == "-h" || arg == "--help" {
+			// 初始化命令行flag与args，环境变量与配置文件
+			cmd.Execute()
+			// 打印信息后直接退出
+			return
+		}
+	}
+	// 初始化命令行flag与args，环境变量与配置文件
 	cmd.Execute()
 	// 启动网页服务器（不阻塞）
 	routers.StartWebServer()
+	// 启动或停止 Tailscale 服务（如启用）
+	routers.StartTailscale()
 	// 扫描书库（命令行指定）
 	cmd.ScanStore(cmd.Args)
 	// 在命令行显示QRCode
