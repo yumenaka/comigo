882d8e8e04e90e4a3bafdc8cd0018fefbcf68dce
yumenaka <www.bailin.tv@gmail.com>
2025-11-12 08:36:47 +0900

    v1.1.0

--- tools/scan/save_to_db_fake.go
+++ tools/scan/save_to_db_fake.go
@@ -1,8 +1,8 @@
-//go:build (windows && 386) || js
+//go:build js
 
 package scan
 
 // SaveBooksToDatabase 4，保存扫描结果到数据库，并清理不存在的书籍
-func SaveResultsToDatabase(ConfigDir string, ClearDatabaseWhenExit bool) error {
+func SaveBooksToDatabase(cfg ConfigInterface) error {
 	return nil
 }
