ddec756dc13ed4687682d6da3f8b1b7e6c7f979a
yumenaka <www.bailin.tv@gmail.com>
2026-02-08 18:31:53 +0900

    更新注释

--- templ/pages/settings/store_config_templ.go
+++ templ/pages/settings/store_config_templ.go
@@ -333,7 +333,7 @@ func StoreConfig(name string, values []string, description string, bookCounts ma
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 26, "\" class=\"w-26 h-10 mx-2 my-1 px-1 border rounded shadow-lg font-semibold focus:outline-none focus:ring transition delay-150 duration-300 ease-in-out\" :class=\"selectedStoreB64 === '' ? 'hidden' : 'bg-red-200  hover:bg-red-400'\">删除书库</button></div></div><div x-text=\"")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 26, "\" class=\"w-26 h-10 mx-2 my-1 px-1 border rounded shadow-lg font-semibold focus:outline-none focus:ring transition delay-150 duration-300 ease-in-out\" :class=\"selectedStoreB64 === '' ? 'hidden' : 'bg-red-200  hover:bg-red-400'\">删除书库</button></div></div><div x-html=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
