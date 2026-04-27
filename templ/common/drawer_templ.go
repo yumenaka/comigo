b85a67fe1f38b30b5d04cada8344fc56d5c25b95
yumenaka <www.bailin.tv@gmail.com>
2026-04-28 01:04:54 +0900

    fix bug

--- templ/common/drawer_templ.go
+++ templ/common/drawer_templ.go
@@ -46,9 +46,9 @@ func Drawer(c echo.Context, book *model.Book, slot templ.Component, showReturnIc
 				return templ_7745c5c3_Err
 			}
 			var templ_7745c5c3_Var2 templ.SafeURL
-			templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinURLErrs(templ.SafeURL(returnUrl))
+			templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinURLErrs(templ.SafeURL(config.PrefixPath(returnUrl)))
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `templ/common/drawer.templ`, Line: 24, Col: 141}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `templ/common/drawer.templ`, Line: 24, Col: 160}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 			if templ_7745c5c3_Err != nil {
